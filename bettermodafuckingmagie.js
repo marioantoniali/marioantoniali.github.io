f1928e48099fe0131be63235b402a1d566ad1147
marioantoniali <mario.antoniali@gmail.com>
2018-06-28 01:26:43 +0200

    ajout psyche mode en JS

--- bettermodafuckingmagie.js
+++ bettermodafuckingmagie.js
@@ -1,1 +1,7 @@
-setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");}, 10000);
+setTimeout(function(){ 
+alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}, 10000);
+
+function magentamode() {
+    document.getElementById("colorswitch").style.backgroundColor = "#FF00FF";
+}
